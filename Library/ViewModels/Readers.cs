0909e2c42ec5ca4d6ee240bffd7771be44311f37
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-04-24 14:02:39 -0400

    playing around some more with bootstrap

---
+++ Library/ViewModels/Readers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.ViewModels
+{
+    public class Readers
+    {
+        public int ID { get; set; }
+        public string ReaderName { get; set; }
+        public string Email { get; set; }
+        public string CardNumber { get; set; }
+        public string Role { get; internal set; }
+    }
+}
